89fcab7c881901e507d02ee091fb8291e140e61b
shecodessometimes <julia@bebopinteractive.com>
2022-10-07 09:03:33 -0400

    Updated the format of the interface.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from Bio.Blast.NCBIWWW import qblast
 
-fasta_file_names = ["sequence"]
+fasta_file_names = ["sequence", "sequence (2)"]
 
 for i in range(len(fasta_file_names)):
     fasta_string = open("fasta_files/"+fasta_file_names[i]+".fasta").read()
-    print("File accessed.")
+    print(f"{fasta_file_names[i]}.fasta was accessed.")
     result_handle = qblast("blastn", "nt", fasta_string)
-    print("File blasted.")
+    print(f"{fasta_file_names[i]}.fasta was blasted.")
 
-    with open("output_files/"+fasta_file_names[i]+"blast.xml", "w") as out_handle:
+    with open("output_files/"+fasta_file_names[i]+"_blast.xml", "w") as out_handle:
         out_handle.write(result_handle.read())
-        print(f"The {fasta_file_names[i]}.fasta file was blasted.")
+        print(f"The blasted {fasta_file_names[i]}.fasta file was written to {fasta_file_names[i]}_blast.xml.")
     result_handle.close()
