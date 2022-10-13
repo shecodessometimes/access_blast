2bf14b2742e78e240522570ab3579de434c70098
shecodessometimes <julia@bebopinteractive.com>
2022-10-13 10:18:50 -0400

    Goes through all the files in a given folder and BLASTs them.

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from Bio.Blast.NCBIWWW import qblast
+import os
 
-fasta_file_names = ["sequence", "sequence (2)"]
+#identify the folder to look for .fasta files in
+foldername = "fasta_files"
+folder = os.fsencode(foldername)
 
-for i in range(len(fasta_file_names)):
-    fasta_string = open("fasta_files/"+fasta_file_names[i]+".fasta").read()
-    print(f"{fasta_file_names[i]}.fasta was accessed.")
-    result_handle = qblast("blastn", "nt", fasta_string)
-    print(f"{fasta_file_names[i]}.fasta was blasted.")
+#blast all the .fasta files within the folder
+for file in os.listdir(folder):
+    #get the file name for a given file
+    filename = os.fsdecode(file)
 
-    with open("output_files/"+fasta_file_names[i]+"_blast.xml", "w") as out_handle:
-        out_handle.write(result_handle.read())
-        print(f"The blasted {fasta_file_names[i]}.fasta file was written to {fasta_file_names[i]}_blast.xml.")
-    result_handle.close()
+    #check if it's a .fasta file
+    if filename.endswith(".fasta"):
+        fasta_string = open(foldername + "/" + filename).read()
+        print(f"{filename} was accessed.")
+
+        #blast the file and put result in result_handle
+        result_handle = qblast("blastn", "nt", fasta_string)
+        print(f"{filename} was blasted.")
+
+        #write the result into a new .xml file
+        with open(f"output_files/{filename[:-6]}_blast.xml", "w") as out_handle:
+            out_handle.write(result_handle.read())
+            print(f"The blasted {filename} file was written to{filename[:-6]}_blast.xml")
+
+        #exit the program
+        result_handle.close()
